e93866e65d515c113b74b7f634d507497f76b8c2
Carson Dickie <carsonmdickie@gmail.com>
2021-04-06 19:11:49 -0400

    Added new target variable (grade group)

--- data_loader.py
+++ data_loader.py
@@ -13,10 +13,32 @@ def load_and_process_data(file_name):
     # Remove the features that only contain 0 values
     data = data.loc[:, (data != 0).any(axis=0)]
     
+    # Add new target variable (GRADE_GROUP)
+    targets = []
+    for index, row in data.iterrows():
+        # GLEASON SCORE = 6 -> GRADE GROUP = 1
+        if (row['GLEASON_SCORE'] == 6):
+            targets.append(1)
+        # GLEASON SCORE = 7 -> GRADE GROUP = 2/3          
+        elif (row['GLEASON_SCORE'] == 7):
+            # PRIMARY SCORE = 3 -> GRADE GROUP = 2
+            if (row['GLEASON_PATTERN_PRIMARY'] == 3):
+                targets.append(2)
+            #PRIMARY SCORE = 4 -> GRADE GROUP = 3
+            else:
+                targets.append(3)
+        # GLEASON SCORE = 8 -> GRADE GROUP = 4              
+        elif (row['GLEASON_SCORE'] == 8):
+            targets.append(4)
+        # GLEASON SCORE = 9/10 -> GRADE GROUP = 5
+        else:
+            targets.append(5)
+    data.insert(len(data.columns), "GRADE_GROUP", targets)
+    
     # Identify independant and dependant variables
     # Remove the last 6 columns
     X = data.iloc[:, : -6]
     # Temporarily setting target variable to 'GLEASON_PATTERN_PRIMARY'
-    y = data.iloc[:, -5]
+    y = data['GRADE_GROUP']
     
     return X, y
