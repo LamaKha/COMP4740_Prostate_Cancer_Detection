7b9bbacbef9a15a5cd2a2a05e86c436e17aab4a1
LamaKha <68357513+LamaKha@users.noreply.github.com>
2021-04-06 20:15:33 -0400

    Update feature_optimization.py

--- feature_optimization.py
+++ feature_optimization.py
@@ -1,13 +1,21 @@
 from sklearn.feature_selection import SelectKBest, chi2
-
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import SelectFromModel
+from sklearn.preprocessing import StandardScaler
+from sklearn.feature_selection import VarianceThreshold
 
 def filter_features(X_train, y_train, X_test, filter, n):
     if (filter == 'chi2'):
         selector = SelectKBest(chi2, k = n)
-    
+    if  (filter == 'VT'):
+        scaler = StandardScaler()
+        scaled_data = scaler.fit_transform(SelectKBest(chi2, k = n))
+        selector = VarianceThreshold(scaled_data)
+    if  (filter == 'RF'):
+        selector = SelectFromModel(RandomForestClassifier(n_estimators=n))
+
     return selector.fit_transform(X_train, y_train), selector.transform(X_test)
 
 def feature_search(X, y):
     return
-
     
