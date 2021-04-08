7238d024ef0e033c55e8f8304dfb425f370c25d7
Carson Dickie <carsonmdickie@gmail.com>
2021-04-07 21:43:20 -0400

    Completed initial version of main_clean and pipeline.py

--- main_clean.py
+++ main_clean.py
@@ -1,94 +1,60 @@
-from sklearn.svm import SVC
-from collections import Counter
-from imblearn.combine import SMOTEENN
-from imblearn.over_sampling import SMOTE
-from sklearn.naive_bayes import GaussianNB
-from data_loader import load_and_process_data
-from sklearn.preprocessing import StandardScaler
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import cross_val_score
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.feature_selection import VarianceThreshold, SelectKBest, chi2, SelectFromModel
-import warnings
-warnings.filterwarnings("ignore")
+import pipeline as pl
 
-SEED = 42
+SEED = 42       # The random seed used
+K_BEST = 15000  # The number of features to select using Chi2 filter
+N_DIM = 3       # The number of dimensions (dimensionality reduction)
+N_FOLDS = 8     # The number of folds to use in cross-fold validation
+ 
+# Load and process the data and return the feature and target matrices -------
+X, y = pl.load_and_process_data('prad_tcga_genes.csv', 'GLEASON_SCORE')
 
-# MAIN PROGRAM ---------------------------------------------------------------
+# Display the data profile ---------------------------------------------------
+pl.data_profile(X, y, 'INITIAL')
 
-# Load and preprocess the data (data_loader.py)
-# THIS FUNCTION SHOULD TAKE A FILE NAME AS A PARAMETER AND RETURN
-# THE SET OF FEATURES (X) AND TARGET VARIABLE (Y)
-x, y = load_and_process_data('prad_tcga_genes.csv')
-y_count = Counter(y)
-print(y_count)
-# Initial Feature Selection Methods--------------------------------------------
+# Perform feature selection --------------------------------------------------
+X_select = pl.feature_selection(X, y, K_BEST)
 
+# Perform up-sampling and down-sampling to address class imbalance -----------
+X_balanced, y_balanced = pl.fix_imbalance(X_select, y, SEED)
 
-#selecting best k-features
-bestfeatures = SelectKBest(score_func=chi2, k=15000)
-fit = bestfeatures.fit(x, y)
-best_x = fit.transform(x)
+# Display the updated data profile -------------------------------------------
+pl.data_profile(X_balanced, y_balanced, 'IMBALANCE ADJUSTED')
 
-# Scaling the DataFrame
-scaler = StandardScaler()
-scaled_data = scaler.fit_transform(best_x)
+# Perform dimensionality reduction (optional) --------------------------------
+X_reduced = pl.reduce_dimensions(X_balanced, y_balanced, N_DIM)
 
-# Eliminating low variance features
-selector = VarianceThreshold()
-var_thres = selector.fit_transform(scaled_data)
-print(var_thres.shape)
+# Perform classification -----------------------------------------------------
+model_names = ['Naive Bayes', 'Support Vector Machine', 'Random Forest', 'K Nearest Neighbors']
+n_models = len(model_names)
+scores = []
+averages = []
+models = []
+for i in range(n_models):
+    m, s, a = pl.classify(X_balanced, y_balanced, model_names[i], N_FOLDS)
+    models.append(m)
+    scores.append(s)
+    averages.append(a)
+    
+# Display results ------------------------------------------------------------
+for i in range(n_models):
+    pl.display_results(model_names[i], scores[i], averages[i])
 
-#Additional Feature Selection Methods---------------------------------------------------------------------------------
+# Perform classification after dimensionality reduction ----------------------
+print("RESULTS AFTER DIMENSIONALITY REDUCTION")
+model_names = ['Naive Bayes', 'Support Vector Machine', 'Random Forest', 'K Nearest Neighbors']
+n_models = len(model_names)
+scores = []
+averages = []
+models = []
+for i in range(n_models):
+    m, s, a = pl.classify(X_reduced, y_balanced, model_names[i], N_FOLDS)
+    models.append(m)
+    scores.append(s)
+    averages.append(a)
+    
+# Display results ------------------------------------------------------------
+for i in range(n_models):
+    pl.display_results(model_names[i], scores[i], averages[i])
 
-#Feature Selection using tree based method
-clf = RandomForestClassifier(n_estimators=100)
-clf = clf.fit(var_thres,y)
-model = SelectFromModel(clf, prefit=True)
-X_new_gleason = model.transform(var_thres)
-print(X_new_gleason.shape)
-
-#Resampling dataset
-sme = SMOTEENN(random_state=SEED, smote=SMOTE(random_state=SEED, k_neighbors=1))
-X_res_smoteenn, y_res_smoteenn = sme.fit_resample(X_new_gleason, y)
-print('Resampling of dataset using SMOTEENN %s' % Counter(y_res_smoteenn), '\n')
-
-lda = LinearDiscriminantAnalysis(n_components = 3)
-X_lda = lda.fit_transform(X_res_smoteenn,y_res_smoteenn)
-print("==============================================================================================================")
-
-#Second Testing of Classifiers After adding more Feature Selection Methods--------------------------------------------
-print("Testing of Classifiers After Applying Additional Feature Selection Methods")
-
-#Naive Bayes on Resampled dataset
-gnb = GaussianNB()
-scores_nb=cross_val_score(gnb, X_res_smoteenn,y_res_smoteenn, scoring='accuracy', cv=8)
-print("Accuracy scores for Gaussian Naive Bayes algorithm on resampled dataset : \n" , scores_nb , '\n')
-print("Mean Accuracy :" , scores_nb.mean())
-print("==============================================================================================================")
-
-# SVM on Resampled dataset
-clf = SVC(gamma='auto')
-scores_svm=cross_val_score(clf, X_res_smoteenn,y_res_smoteenn, scoring='accuracy', cv=8)
-print("Accuracy scores for SVM-RBF algorithm on resampled dataset : \n" , scores_svm , '\n')
-print("Mean Accuracy :" , scores_svm.mean())
-print("==============================================================================================================")
-
-#Random Forest on Resampled dataset
-rf = RandomForestClassifier(n_estimators=100,n_jobs=-1,max_depth=10,max_features='auto')
-scores_rf=cross_val_score(rf, X_res_smoteenn,y_res_smoteenn, scoring='accuracy', cv=8)
-print("Accuracy scores for Random Forest algorithm on resampled dataset : \n" , scores_rf , '\n')
-print("Mean Accuracy :" , scores_rf.mean())
-print("==============================================================================================================")
-
-#KNN neighbours on resampled dataset
-k_scores = []
-# Calculating best values for K values between 1 and 20
-for i in range(1, 21):
-    knn = KNeighborsClassifier(n_neighbors=i)
-    scores_knn = cross_val_score(knn, X_res_smoteenn, y_res_smoteenn, cv=8, scoring='accuracy')
-    k_scores.append(scores_knn.mean())
-print("Mean Accuracy for each K belonging to  {1:21} : \n" , k_scores , '\n')
-print("Best of the Mean Accuracies :" , max(k_scores))
-print("==============================================================================================================")
+    
+    
---
+++ pipeline.py
@@ -0,0 +1,215 @@
+import pandas as pd
+from collections import Counter
+from sklearn.svm import SVC
+from imblearn.combine import SMOTEENN
+from imblearn.over_sampling import SMOTE
+from sklearn.naive_bayes import GaussianNB
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import cross_val_score, KFold
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.feature_selection import VarianceThreshold, SelectKBest, chi2, SelectFromModel
+
+
+def load_and_process_data(file_name, target):
+    """
+    load_and_process_data reads a csv from the working directory, 
+    transposes the resulting dataframe (so the rows of the original CSV 
+    become the features), removes any features that contain 0 values, adds
+    GRADE_GROUP as a dependant variable (if desired) and returns the 
+    feature matrix and target variable matrix (which can be set by the user).
+
+    :param file_name: the name of the file ['example.csv'] (string)
+    :param target: the name of the row containing the dependant variable (string)
+    :return: X [the feature matrix] and y [the target matrix] (both numpy arrays)
+    """ 
+    data = pd.read_csv(file_name)
+
+    # Set index to ID values (otherwise keys are wrong after transposition)
+    data = data.set_index('ID')
+
+    # Transpose the data (because features are rows in CSV and they should be columns)
+    data = data.transpose()
+
+    # Remove the features that only contain 0 values
+    data = data.loc[:, (data != 0).any(axis=0)]
+
+    # Add new target variable (GRADE_GROUP)
+    if (target == 'GRADE_GROUP'):
+        targets = []
+        for index, row in data.iterrows():
+            # GLEASON SCORE = 6 -> GRADE GROUP = 1
+            if (row['GLEASON_SCORE'] == 6):
+                targets.append(1)
+            # GLEASON SCORE = 7 -> GRADE GROUP = 2/3
+            elif (row['GLEASON_SCORE'] == 7):
+                # PRIMARY SCORE = 3 -> GRADE GROUP = 2
+                if (row['GLEASON_PATTERN_PRIMARY'] == 3):
+                    targets.append(2)
+                # PRIMARY SCORE = 4 -> GRADE GROUP = 3
+                else:
+                    targets.append(3)
+            # GLEASON SCORE = 8 -> GRADE GROUP = 4
+            elif (row['GLEASON_SCORE'] == 8):
+                targets.append(4)
+            # GLEASON SCORE = 9/10 -> GRADE GROUP = 5
+            else:
+                targets.append(5)
+        data.insert(len(data.columns), "GRADE_GROUP", targets)
+        # Remove the last 6 columns from the feature matrix
+        X = data.iloc[:, : -6]
+    else:
+        # Remove the last 5 columns from the feature matrix
+        X = data.iloc[:, : -5]        
+
+    # Set the target variable matrix
+    y = data[target]
+
+    return X, y
+
+def print_divider():
+    """
+    print_divider prints a dividing line (===).
+    """ 
+    print("==============================================================================================================")
+
+
+def data_profile(X, y, phase):
+    """
+    data_profile displays key components of the data to the user.
+
+    :param X: the feature  (numpy array)
+    :param y: the target matrix (numpy array)
+    :param phase: the current phase of data processing (string) 
+    :return: None
+    """ 
+    print_divider()
+    print("DATA PROFILE - ", phase)
+    print("------------")
+    tally = Counter(y)
+    print("Number of samples: ", X.shape[0])
+    print("Number of features: ", X.shape[1])
+    print("Class Distribution:")
+    print(tally)
+    print_divider()   
+    
+def feature_selection(X, y, n):
+    """
+    feature_selection performs a series of feature selection steps on the 
+    data. 
+    load_and_process_data reads a csv from the working directory, 
+    transposes the resulting dataframe (so the rows of the original CSV 
+    become the features), removes any features that contain 0 values, adds
+    GRADE_GROUP as a dependant variable (if desired) and returns the 
+    feature matrix and target variable matrix (which can be set by the user).
+
+    :param file_name: the name of the file ('example.csv')
+    :param target: the name of the row containing the dependant variable
+    :return: X (the feature matrix) and y (the target matrix)
+    """ 
+    # Apply Chi2 filter to reduce eliminate unimportant features
+    bestfeatures = SelectKBest(score_func=chi2, k=n)
+    fit = bestfeatures.fit(X, y)
+    X_filtered = fit.transform(X)
+    
+    # Normalize the remaining features
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X_filtered)
+
+    # Eliminate features that do not various accross the population
+    selector = VarianceThreshold()
+    X_threshold = selector.fit_transform(X_scaled)
+    
+    # Apply 2nd round of feature selection using a random forest
+    clf = RandomForestClassifier(n_estimators=100)
+    clf = clf.fit(X_threshold, y)
+    model = SelectFromModel(clf, prefit=True)
+    X_forest = model.transform(X_threshold)
+    
+    return X_forest
+
+def fix_imbalance(X, y, seed):
+    sme = SMOTEENN(random_state=seed, smote=SMOTE(random_state=seed, k_neighbors=1))
+    X_smoteen, y_smoteen = sme.fit_resample(X, y)
+    
+    return X_smoteen, y_smoteen    
+    
+def reduce_dimensions(X, y, n_dim):
+    lda = LinearDiscriminantAnalysis(n_components = n_dim)
+    X_lda = lda.fit_transform(X, y)
+    
+    return X_lda
+
+def classify(X, y, classifier, n_folds, hyperparameters = None):   
+    # Naive Bayes Classifier
+    if (classifier == 'Naive Bayes'):
+        model = GaussianNB()
+    # Support Vector Machine Classifier:
+    if (classifier == 'Support Vector Machine'):
+        model = SVC()    
+    # Random Forest Classifier
+    if (classifier == 'Random Forest'):
+        model = RandomForestClassifier(n_jobs = -1, max_depth = 10)
+    # K Nearest Neighbors Classifier
+    if (classifier == 'K Nearest Neighbors'):
+        k = find_best_k(X, y, n_folds)
+        model = KNeighborsClassifier(n_neighbors = k)
+    # Perform cross-validation and obtain set of 10 accuracy values
+    scores = cross_val_score(model, X, y, scoring = 'accuracy', cv = n_folds)
+    # Find the average accuracy
+    average = scores.mean()
+    
+    return model, scores, average
+    
+def find_best_k(X, y, k):  
+    """
+    find_best_k takes an array of feature data from a dataset (in this case 
+    there are two features, x1 and x2), an array of corresponding lables identifying 
+    each datapoint (as either belonging to class 0 or class 1 in this case) 
+    and the number of folds (used in k-fold cross validation). It then runs a k-NN
+    classifier using values ranging from 1-20 for 'k' and returns the value of 'k' 
+    that achieved the best results (with respect to accuracy).
+
+    :param X: the feature matrix
+    :param y: the labels vector
+    :param k: the number of folds used in k-fold cross validation
+    :return: void (output is sent to the display)
+    """ 
+    kf = KFold(n_splits=k)
+    
+    range_k = range(1, 20)
+    scores_list = []
+    for k in range_k:
+        model = KNeighborsClassifier(n_neighbors=k)
+        score = cross_val_score(model, X, y, cv = kf)
+        scores_list.append(score.mean())
+    
+    # OPTIONAL PLOTTING
+    #plt.plot(range_k, scores_list)
+    #plt.xlabel("Value of K")
+    #plt.ylabel("Accuracy")
+    #plt.show()
+    
+    max_accuracy = max(scores_list)
+    best_k = scores_list.index(max_accuracy) + 1
+    
+    return best_k
+
+def display_results(model_name, scores, average):
+    print_divider()
+    print("Model: ", model_name)
+    print("Accuracy Scores: ")
+    print(scores)
+    print("Average accuracy: ", average)
+    print_divider()
+  
+
+    
+    
+    
+    
+    
+    
+    
+    
